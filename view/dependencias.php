e0607bcf1f77ed8450aaba0a627a57e8d110cd13
LucasLange <lucaslange@rede.ulbra.br>
2022-05-19 20:27:16 -0300

    mvc_php_js

---
+++ view/dependencias.php
@@ -0,0 +1,27 @@
+<meta charset="UTF-8">
+
+<!-- Titulo e Icone - HEAD -->
+<title>CRUD</title>
+
+<link rel="shortcut icon" href="images/icon.png" type="image/x-icon">
+
+<!-- CDN's -->
+
+<!-- Bootstrap 4 -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<!-- Fontawesome 5 -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+<!-- GoogleFonts - OpenSans -->
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+
+<link href="../assets/styles.css" rel="stylesheet">
+
+<!-- CDN's - JQuery e JQueryMask - Caso Seja Necessário, incluir na Página.
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.15/jquery.mask.min.js"></script> 
+
+-->
